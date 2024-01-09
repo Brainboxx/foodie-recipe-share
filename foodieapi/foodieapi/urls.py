fc43903cc59500e4359328db27b7d81bc537c8e3
Chinedu Aja <72220227+Brainboxx@users.noreply.github.com>
2024-01-09 09:18:40 +0100

    Update urls.py

--- foodieapi/foodieapi/urls.py
+++ foodieapi/foodieapi/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
+    path('api/users', include('users.urls')),
     path('api/recipes/', include('recipe.urls')),
 ]
 
